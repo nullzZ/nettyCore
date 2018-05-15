88b26af56cc04c2f3a8d3bf3008946ad135b492d
nullzZ <maleivip1988@163.com>
2018-05-15 15:37:57 +0800

    update

--- src/main/java/game/core/nettyCore/http/HttpHandler.java
+++
@@ -1,7 +0,0 @@
-package game.core.nettyCore.http;
-
-import io.netty.channel.ChannelHandlerContext;
-
-public interface HttpHandler<R extends ChannelHandlerContext, M, S> extends IHttpHandler<R, M, S> {
-
-}
